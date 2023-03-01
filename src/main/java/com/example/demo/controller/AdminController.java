66ec08c55e18fd8b368902392516d6d4319e5653
Karan-PS <114496979+Karan-PS@users.noreply.github.com>
2023-03-01 21:01:56 +0530

    Update AdminController.java

--- src/main/java/com/example/demo/controller/AdminController.java
+++ src/main/java/com/example/demo/controller/AdminController.java
@@ -3,7 +3,9 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -108,6 +110,13 @@ public class AdminController {
 	public List<StudentAllotedEntity> displayAllotedStudents() {
 		 List<StudentAllotedEntity> list = studentAllotedRepository.findAll();
 		return list;	
+	}
+	
+	//Delete Alloted Student 
+	@DeleteMapping("/deleteAllotedStudent/{id}")
+	public String deleteAllotedStudent(@PathVariable int id) {
+		studentAllotedRepository.deleteById(id);
+		return "Deleted : Enter your new Course and Marks";
 	}
 		
 }
